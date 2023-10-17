a86a0acdcad97927cc3d8314f5f044966571cccd
ajaykumarbk75 <71759022+ajaykumarbk75@users.noreply.github.com>
2023-10-18 02:19:01 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("********** -( Hi HPE Team good evening, My Name is '*****', We are learning AZURE Cloud Computing[AZ900] ) **************** ")
+    return HttpResponse("********** -( Hi HPE Team good evening, We are learning AZURE Cloud Computing[AZ900] ) **************** ")
+    return HttpResponse(" The Azure cloud platform is more than 200 products and cloud services designed to help you bring new solutions to life ")
