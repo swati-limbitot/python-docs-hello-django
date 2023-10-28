e7ac1a360915b03fccf18a4d370a1681d72f56e4
ajaykumarbk75 <71759022+ajaykumarbk75@users.noreply.github.com>
2023-10-28 12:49:47 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("********** -( Hi HPE Team good evening, We are learning AZURE Cloud Computing[AZ900] ) **************** ")
+    return HttpResponse("********** -( Hi Team good Afternoon, Today we are happy ~!! ) **************** ")
  
