373fbfe7ff350c1e2804283b51c47991df991015
ajaykumarbk75 <71759022+ajaykumarbk75@users.noreply.github.com>
2024-07-04 16:14:52 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("********** -( Hi Team good Afternoon, Today we are happy ~!! ) **************** ")
+    return HttpResponse("********** -( Hi Team good Evening, Today we are happy & its beatifull day ~!! ) **************** ")
  
