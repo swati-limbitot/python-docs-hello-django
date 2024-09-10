605151e343e9f9568528c2859c1b7681513ed8fe
ajaykumarbk75 <71759022+ajaykumarbk75@users.noreply.github.com>
2024-09-10 15:47:43 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("********** -( Hi Microland Team, we are learning App-Architecture ~!! )-**************** ")
+    return HttpResponse("********** -( Hi Microland Team, we are working on CI/CD part ~!! )-**************** ")
  
